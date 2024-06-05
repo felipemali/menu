8678d0dafc4a393e795fd62f28fa7805680776a4
Felipe Lima <felipedesouzamali@gmail.com>
2024-06-05 01:26:14 -0400

    change css menu

--- src/components/Menu/css.ts
+++ src/components/Menu/css.ts
@@ -2,10 +2,8 @@ export const css = {
   container: {
     display: "flex",
     flexDirection: "column",
-    height: "100vh",
-    minHeight: "100vh",
-    overflow: "hidden",
-    paddingBottom: "10rem",
+    height: "500px",
+    overflowY: "auto",
   },
   appBar: {
     background: "linear-gradient(90deg, #220e03f8 8%, #4d1f04f8 95%)",
@@ -17,10 +15,9 @@ export const css = {
     justifyContent: "center",
     flexShrink: 0,
     zIndex: 1000,
+    overflowY: "auto",
   },
   contentContainer: {
-    flex: 1,
-    overflowY: "auto",
     background: "linear-gradient(90deg, #220e03f8 8%, #4d1f04f8 95%)",
     borderRadius: "0 0 25px 25px",
     paddingTop: "10px",
