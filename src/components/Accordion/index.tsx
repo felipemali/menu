6a2cee9dbb3f9c44ca6b5831aafe0b3d505b4c3d
Felipe Lima <felipedesouzamali@gmail.com>
2024-06-04 23:14:23 -0400

    teste

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -3,7 +3,7 @@ import { Accordion, AccordionDetails, Box, Typography } from "@mui/material/";
 import { MenuContext } from "../../context/MenuContext";
 import { useContext } from "react";
 // import { useOrderResume } from "../../hooks/useOrderResume";
-
+//teste
 const ArccodionOrder = () => {
   const { ordersPlaced } = useContext(MenuContext);
 
